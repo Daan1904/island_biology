d56c8f1b0a20968ae2b8f63df7d7bd1d6b65088f
Daan <d.kroeze.1@student.rug.nl>
2025-02-03 10:07:23 +0100

    Project initialization

---
+++ scripts/00_start.R
@@ -0,0 +1,10 @@
+renv::restore()
+
+install.packages('ape',dependencies = TRUE)
+install.packages('phytools',dependencies = TRUE)
+install.packages('DAISIE',dependencies = TRUE)
+install.packages('DAISIEprep',dependencies = TRUE)
+install.packages("paran")
+install.packages("splits", repos="http://R-Forge.R-project.org")
+
+install.packages("tidyverse")
