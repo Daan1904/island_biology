1cfed706e6bf9e451c3caa00a33d7cd233212e05
Daan <d.kroeze.1@student.rug.nl>
2025-02-05 09:44:05 +0100

    Initialized files

---
+++ scripts/03_practical_2.R
@@ -0,0 +1,9 @@
+renv::restore()
+
+install.packages('DAISIE',dependencies = TRUE)
+install.packages('DAISIEprep',dependencies = TRUE)
+install.packages('ape',dependencies = TRUE)
+
+library(DAISIE)
+library(ape)
+library(DAISIEprep)
