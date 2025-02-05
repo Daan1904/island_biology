d42229c31f2c4e8c2b14d95978ce0e13cd5f7a60
Daan <d.kroeze.1@student.rug.nl>
2025-02-05 12:49:50 +0100

    Prepared the practical script

--- scripts/03_practical_2.R
+++ scripts/03_practical_2.R
@@ -7,3 +7,39 @@ install.packages('ape',dependencies = TRUE)
 library(DAISIE)
 library(ape)
 library(DAISIEprep)
+
+rm(list = ls())
+
+###Questions to answer in the report
+##How many times has Insula colonised the Caribbean islands?
+#
+
+##How many times has Insula colonised Jamaica?
+#
+
+##How many radiations have occurred on Jamaica?
+#
+
+##What are the rates of colonisation, speciation and extinction for Insula in Jamaica?
+#
+
+##Is there evidence for diversity-dependence in the Insula species of Jamaica?
+#
+
+##Is there evidence for equilibrium dynamics on the island of Jamaica?
+#
+
+##How has the diversity of Insula on Jamaica changed through time (according to DAISIE simulations)?
+#
+
+##Modify one or two parameters of your choice and run simulations again under those parameters. How does this affect the simulations? Describe.
+#
+
+
+
+
+
+
+
+
+
